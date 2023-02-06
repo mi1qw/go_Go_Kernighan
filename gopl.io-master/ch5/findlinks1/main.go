c26c7b3c7860979a91a8a2c451970393dd2ceab5
mi1qw <123mib@gmail.com>
2023-02-06 03:38:26 +0200

    code book

---
+++ gopl.io-master/ch5/findlinks1/main.go
@@ -0,0 +1,81 @@
+// Copyright © 2016 Alan A. A. Donovan & Brian W. Kernighan.
+// License: https://creativecommons.org/licenses/by-nc-sa/4.0/
+
+// See page 122.
+//!+main
+
+// Findlinks1 prints the links in an HTML document read from standard input.
+package main
+
+import (
+	"fmt"
+	"golang.org/x/net/html"
+	"os"
+)
+
+func main() {
+	doc, err := html.Parse(os.Stdin)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "findlinks1: %v\n", err)
+		os.Exit(1)
+	}
+	fmt.Printf("%#v", *doc)
+	println()
+	for _, link := range visit(nil, doc) {
+		//		fmt.Println(link)
+		fmt.Printf("=========%#v\n", link)
+	}
+}
+
+//!-main
+
+// !+visit
+// visit appends to links each link found in n and returns the result.
+func visit(links []string, n *html.Node) []string {
+	fmt.Printf("----%#v\n", *n)
+	if n.Type == html.ElementNode && n.Data == "a" {
+		fmt.Printf("%#v", *n)
+		for _, a := range n.Attr {
+			if a.Key == "href" {
+				println(a.Val, "href !!!!!!!")
+				links = append(links, a.Val)
+			}
+		}
+	}
+	for c := n.FirstChild; c != nil; c = c.NextSibling {
+		links = visit(links, c)
+	}
+	return links
+}
+
+//!-visit
+
+/*
+//!+html
+package html
+
+type Node struct {
+	Type                    NodeType
+	Data                    string
+	Attr                    []Attribute
+	FirstChild, NextSibling *Node
+}
+
+type NodeType int32
+
+const (
+	ErrorNode NodeType = iota
+	TextNode
+	DocumentNode
+	ElementNode
+	CommentNode
+	DoctypeNode
+)
+
+type Attribute struct {
+	Key, Val string
+}
+
+func Parse(r io.Reader) (*Node, error)
+//!-html
+*/
