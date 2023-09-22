283c0da8ef75d1db93816cbd3e2a1d5ba6887b5d
mi1qw <123mib@gmail.com>
2023-09-22 23:19:39 +0300

    isclosed

---
+++ multithread/cannel/isClosed.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+type T int
+
+func IsClosed(ch <-chan T) bool {
+	select {
+	case <-ch:
+		return true
+	default:
+	}
+
+	return false
+}
+
+func main() {
+	c := make(chan T)
+	fmt.Println(IsClosed(c)) // false
+	close(c)
+	fmt.Println(IsClosed(c)) // true
+}
